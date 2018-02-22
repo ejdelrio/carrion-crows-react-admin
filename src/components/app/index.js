10b45dc23b6c4a49fb3c05873c3a9b3a9e97ddd8
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-21 18:03:51 -0800

    Setup React environment :D

---
+++ src/components/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <section>
+        <h1>{'Hello World'}</h1>
+      </section>
+    );
+  }
+}
+
+export default App;
+
