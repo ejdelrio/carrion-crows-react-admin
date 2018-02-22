674b746a0c8ab3819d52c19d172c229a40a7e86a
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-21 18:41:08 -0800

    Created function for API calls for authentication :D

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -10,6 +10,17 @@ export const tokenDelete = () => {
   return {type: 'TOKEN_DELETE'};
 };
 
+export const login = user => dispatch => {
+  let {userName, passWord} = user;
+  return superagent.get(`${__API_URL__}/api/login`)
+  .auth(userName, passWord)
+  .then(res => {
+    util.createCookie('pingme-token', res.text, 1);
+    dispatch(tokenSet(JSON.parse(res.text)));
+    return res;
+  })
+}
+
 
 
 export const signup = user => dispatch => {
