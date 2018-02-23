594ab81640ce832ebb6369da52e86b6ebdb069d4
Eddie <Eddie@Edwins-MBP.lan>
2018-02-23 14:55:44 -0800

    Created bandMember reducer :D

---
+++ src/reducers/bandMember.js
@@ -0,0 +1,18 @@
+
+
+module.exports = (state=[], action) => {
+  let {type, payload} = action;
+
+  switch(type) {
+    case 'MEMBER_FETCH':
+      return payload;
+    case 'MEMBER_CREATE':
+      return [...state, payload];
+    case 'MEMBER_DELETE':
+      return state.filter(val => val._id === payload._id);
+    case 'MEMBER_UPDATE':
+      return state.map(val => val._id === payload._id ? payload : val);
+    default:
+      return state;
+  }
+}
